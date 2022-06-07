21ba990c70841ff379118b03e153436dae61ec50
alejandro <amondragonflrs@gmail.com>
2022-06-06 23:20:42 -0500

    Add players attribute

--- tictactoe/board.py
+++ tictactoe/board.py
@@ -1,3 +1,4 @@
 class Board:
-    def __init__(self):
+    def __init__(self, players: list = None):
         self.state = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        self.players = players if players is not None else ["x", "o"]
