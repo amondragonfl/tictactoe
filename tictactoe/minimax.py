5d3253728b1e51c79936b00ccbb450cd0bb643d4
alejandro <amondragonflrs@gmail.com>
2022-06-07 11:42:23 -0500

    Add static_eval logic

--- tictactoe/minimax.py
+++ tictactoe/minimax.py
@@ -2,7 +2,7 @@ from board import Board
 
 
 class Minimax:
-    def __int__(self, board: Board, player: str):
+    def __init__(self, board: Board, player: str):
         if not board.player_exists(player):
             raise ValueError(f"Invalid player: {player}")
         self.player = player
@@ -11,5 +11,11 @@ class Minimax:
     def best_move(self):
         pass
 
-    def static_eval(self):
-        pass
+    def static_eval(self) -> int:
+        empty = self.board.count_empty()
+        if self.board.get_winner() is None:
+            return 0
+        elif self.board.get_winner() == self.player:
+            return empty
+        else:
+            return -empty
