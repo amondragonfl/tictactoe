1af2cc5f380161ee2e67d0be4491fb0ce929143d
alejandro <amondragonflrs@gmail.com>
2022-06-07 13:34:02 -0500

    Implemented minimax algorithm

--- tictactoe/minimax.py
+++ tictactoe/minimax.py
@@ -9,6 +9,26 @@ class Minimax:
         self.player = player
         self.board = board
 
+    def best_move(self, maximizing_player):
+        if self.board.is_game_over():
+            return self.static_eval(), None
+        if maximizing_player:
+            max_eval = (-math.inf, None)
+            for index in self.board.empty_indices():
+                self.board.make_move(index, "o")
+                current_eval = self.best_move(False)
+                max_eval = (current_eval[0], index) if current_eval[0] > max_eval[0] else max_eval
+                self.board.state[index] = str(index + 1)
+            return max_eval
+        else:
+            min_eval = (math.inf, None)
+            for index in self.board.empty_indices():
+                self.board.make_move(index, "x")
+                current_eval = self.best_move(True)
+                min_eval = (current_eval[0], index) if current_eval[0] < min_eval[0] else min_eval
+                self.board.state[index] = str(index + 1)
+            return min_eval
+
     def static_eval(self) -> int:
         empty = self.board.count_empty()
         if self.board.get_winner() is None:
@@ -17,3 +37,4 @@ class Minimax:
             return empty + 1
         else:
             return -empty - 1
+        
