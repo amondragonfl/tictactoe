7dc72fa017e9e4243d98271f678b23b7175f4015
alejandro <amondragonflrs@gmail.com>
2022-06-07 16:19:34 -0500

    Finish basic game

--- tictactoe/game.py
+++ tictactoe/game.py
@@ -1,4 +1,5 @@
 from board import Board
+from minimax import Minimax
 import os
 
 
@@ -8,20 +9,29 @@ def clear_console():
 
 def main():
     board = Board()
+    ai_player = Minimax(board, "o")
 
     while not board.is_game_over():
         clear_console()
         board.display_board()
         while True:
             try:
-                player_choice = int(input("Enter a number from 1-9: ")) -1
+                player_choice = int(input("Enter a number from 1-9: ")) - 1
             except ValueError:
                 continue
             if player_choice <= 9 and board.is_empty(player_choice):
                 board.make_move(player_choice, "x")
                 break
-        clear_console()
-        board.display_board()
+        if not board.is_game_over():
+            print("Computer is thinking...")
+            board.make_move(ai_player.best_move(True)[1], "o")
+
+    clear_console()
+    board.display_board()
+    if board.count_empty() == 0:
+        print("Tie!")
+        return
+    print(f"You lose!")
 
 
 if __name__ == "__main__":
