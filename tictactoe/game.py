f6b1a308723a3b12247eede36ea3b0fc020a63a8
alejandro <amondragonflrs@gmail.com>
2022-06-07 11:12:03 -0500

    Handle user input

--- tictactoe/game.py
+++ tictactoe/game.py
@@ -1,11 +1,27 @@
 from board import Board
+import os
+
+
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def main():
     board = Board()
 
     while not board.is_game_over():
-        pass
+        clear_console()
+        board.display_board()
+        while True:
+            try:
+                player_choice = int(input("Enter a number from 1-9: ")) -1
+            except ValueError:
+                continue
+            if player_choice <= 9 and board.is_empty(player_choice):
+                board.make_move(player_choice, "x")
+                break
+        clear_console()
+        board.display_board()
 
 
 if __name__ == "__main__":
