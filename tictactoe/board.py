eb2dc79fc30e633c23b88d020d2e4427e1825f24
alejandro <amondragonflrs@gmail.com>
2022-06-06 23:29:39 -0500

    Add make_move method

--- tictactoe/board.py
+++ tictactoe/board.py
@@ -2,3 +2,9 @@ class Board:
     def __init__(self, players: list = None):
         self.state = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
         self.players = players if players is not None else ["x", "o"]
+
+    def make_move(self, index: int, player: str) -> None:
+        if player not in self.players:
+            raise ValueError(f"Invalid player : {player}")
+        if self.state[index] not in self.players:
+            self.state[index] = player
