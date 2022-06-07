f7725e75fa8e12a093146661518f0753cc863337
alejandro <amondragonflrs@gmail.com>
2022-06-06 23:46:12 -0500

    Add check_win method

--- tictactoe/board.py
+++ tictactoe/board.py
@@ -8,3 +8,21 @@ class Board:
             raise ValueError(f"Invalid player : {player}")
         if self.state[index] not in self.players:
             self.state[index] = player
+
+    def check_win(self, player: str) -> bool:
+        if player not in self.players:
+            raise ValueError(f"Invalid player : {player}")
+        win_conditions = [
+            [self.state[0], self.state[1], self.state[2]],
+            [self.state[3], self.state[4], self.state[5]],
+            [self.state[5], self.state[7], self.state[8]],
+            [self.state[0], self.state[3], self.state[6]],
+            [self.state[1], self.state[4], self.state[7]],
+            [self.state[2], self.state[5], self.state[8]],
+            [self.state[0], self.state[4], self.state[8]],
+            [self.state[6], self.state[4], self.state[2]]
+        ]
+        if [player, player, player] in win_conditions:
+            return True
+        return False
+    
